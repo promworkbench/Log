48c6b1a04c764d2086d8dd20371bece0cc55d61d
hmwverbeek <h.m.w.verbeek@tue.nl>
2014-11-17 17:20:58 +0000

    Added new filter that adds artifical start and end events. Each of these events uses the XClassExtension (class:name attribute) to assign class name. These events also copy the global attributes.
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/Log/Trunk@16904 a68f48d4-8d00-424f-b812-3f22a534a02a

---
+++ src/org/processmining/plugins/log/logfilters/impl/AddArtificialStartEndFilter.java
@@ -0,0 +1,80 @@
+package org.processmining.plugins.log.logfilters.impl;
+
+import org.deckfour.xes.extension.std.XClassExtension;
+import org.deckfour.xes.factory.XFactory;
+import org.deckfour.xes.factory.XFactoryRegistry;
+import org.deckfour.xes.info.XLogInfoFactory;
+import org.deckfour.xes.model.XAttribute;
+import org.deckfour.xes.model.XEvent;
+import org.deckfour.xes.model.XLog;
+import org.deckfour.xes.model.XTrace;
+import org.processmining.contexts.uitopia.annotations.UITopiaVariant;
+import org.processmining.framework.plugin.PluginContext;
+import org.processmining.framework.plugin.annotations.Plugin;
+import org.processmining.framework.plugin.annotations.PluginVariant;
+import org.processmining.plugins.log.logfilters.LogFilter;
+import org.processmining.plugins.log.logfilters.LogFilterException;
+import org.processmining.plugins.log.logfilters.XTraceEditor;
+
+@Plugin(name = "Add Artifical Start and End Events", parameterLabels = { "Log", "Parameters" }, returnLabels = { "Log (start and end events added)" }, returnTypes = { XLog.class })
+public class AddArtificialStartEndFilter {
+
+	@UITopiaVariant(affiliation = UITopiaVariant.EHV, author = "H.M.W. Verbeek", email = "h.m.w.verbeek@tue.nl")
+	@PluginVariant(requiredParameterLabels = { 0 }, variantLabel = "Default parameters")
+	public XLog filterDefault(PluginContext context, XLog log) throws LogFilterException {
+		return filterParameters(context, log, new AddArtificialStartEndParameters());
+	}
+
+	@PluginVariant(requiredParameterLabels = { 0, 1 }, variantLabel = "Provided parameters")
+	public XLog filterParameters(PluginContext context, final XLog log, final AddArtificialStartEndParameters parameters)
+			throws LogFilterException {
+		/**
+		 * Add for every trace a start and end event. Classes for these events
+		 * will be set using the classifier extension. All classifiers in the
+		 * log will be wrapped in a priority classifier, where the first
+		 * classifier is the classifier classifier, and the second is the
+		 * original classifier. As a result, the start and end events will be
+		 * classified using the classifier classifier, and the other events
+		 * using the original classifier (assuming that they do not contain the
+		 * classifier:class attribute).
+		 */
+
+		final XFactory factory = XFactoryRegistry.instance().currentDefault();
+
+		/*
+		 * Add a new start and end event in every trace. The start and end event
+		 * use the Classify Extension to store their class names.
+		 */
+		XLog filteredLog = LogFilter.filter(context.getProgress(), 100, log, XLogInfoFactory.createLogInfo(log),
+				new XTraceEditor() {
+
+					public XTrace editTrace(XTrace trace) {
+						if (parameters.isAddStartEvent()) {
+							XEvent startEvent = factory.createEvent();
+							/*
+							 * Set the class name for this event.
+							 */
+							XClassExtension.instance().assignName(startEvent, parameters.getStartClassName());
+							/*
+							 * Copy in the global attributes.
+							 */
+							for (XAttribute attribute: log.getGlobalEventAttributes()) {
+								startEvent.getAttributes().put(attribute.getKey(), attribute);
+							}
+							trace.add(0, startEvent);
+						}
+						if (parameters.isAddEndEvent()) {
+							XEvent endEvent = factory.createEvent();
+							XClassExtension.instance().assignName(endEvent, parameters.getEndClassName());
+							for (XAttribute attribute: log.getGlobalEventAttributes()) {
+								endEvent.getAttributes().put(attribute.getKey(), attribute);
+							}
+							trace.add(endEvent);
+						}
+						return trace;
+					}
+				});
+
+		return filteredLog;
+	}
+}
---
+++ src/org/processmining/plugins/log/logfilters/impl/AddArtificialStartEndParameters.java
@@ -0,0 +1,63 @@
+package org.processmining.plugins.log.logfilters.impl;
+
+public class AddArtificialStartEndParameters {
+
+	/*
+	 * Class name for start events.
+	 */
+	private String startClassName;
+	/*
+	 * Class names for end events.
+	 */
+	private String endClassName;
+	/*
+	 * Whether to add start events.
+	 */
+	private boolean addStartEvent;
+	/*
+	 * Whether to add end events.
+	 */
+	private boolean addEndEvent;
+	
+	public AddArtificialStartEndParameters() {
+		/*
+		 * Default settings.
+		 */
+		setStartClassName("|start>");
+		setEndClassName("[end]");
+		setAddStartEvent(true);
+		setAddEndEvent(true);
+	}
+
+	public String getStartClassName() {
+		return startClassName;
+	}
+
+	public void setStartClassName(String startClassName) {
+		this.startClassName = startClassName;
+	}
+
+	public String getEndClassName() {
+		return endClassName;
+	}
+
+	public void setEndClassName(String endClassName) {
+		this.endClassName = endClassName;
+	}
+
+	public boolean isAddStartEvent() {
+		return addStartEvent;
+	}
+
+	public void setAddStartEvent(boolean addStartEvent) {
+		this.addStartEvent = addStartEvent;
+	}
+
+	public boolean isAddEndEvent() {
+		return addEndEvent;
+	}
+
+	public void setAddEndEvent(boolean addEndEvent) {
+		this.addEndEvent = addEndEvent;
+	}
+}
