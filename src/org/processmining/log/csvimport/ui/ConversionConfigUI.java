e720c9edec302f9abe4fdff4a77f8f5671a8de1e
fmannhardt <f.mannhardt@tue.nl>
2015-06-30 07:47:06 +0000

    Prepared automatic date format guessing
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/Log/Trunk@22558 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/log/csvimport/ui/ConversionConfigUI.java
+++ src/org/processmining/log/csvimport/ui/ConversionConfigUI.java
@@ -6,8 +6,13 @@ import java.awt.event.ActionListener;
 import java.awt.event.AdjustmentEvent;
 import java.awt.event.AdjustmentListener;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.GroupLayout;
@@ -23,8 +28,8 @@ import javax.swing.event.ListDataListener;
 
 import org.processmining.framework.util.ui.widgets.ProMComboBox;
 import org.processmining.framework.util.ui.widgets.ProMListSortableWithComboBox;
-import org.processmining.log.csv.ICSVReader;
 import org.processmining.log.csv.CSVFile;
+import org.processmining.log.csv.ICSVReader;
 import org.processmining.log.csv.config.CSVConfig;
 import org.processmining.log.csvimport.config.CSVConversionConfig;
 import org.processmining.log.csvimport.config.CSVConversionConfig.CSVMapping;
@@ -41,7 +46,43 @@ import com.google.common.collect.Lists;
  */
 public final class ConversionConfigUI extends CSVConfigurationPanel implements AutoCloseable {
 
-	private static final int COLUMN_WIDTH = 360;	
+	private static final int COLUMN_WIDTH = 360;
+
+	private static final int DATA_TYPE_FORMAT_AUTO_DETECT_NUM_LINES = 100;
+	
+	private static final Set<String> CASE_COLUMN_IDS = new HashSet<String>() {
+		private static final long serialVersionUID = 1113995381788343439L;
+	{
+		add("case");
+		add("trace");
+		add("traceid");
+		add("caseid");
+	}};
+	
+	private static final Set<String> EVENT_COLUMN_IDS = new HashSet<String>() {
+		private static final long serialVersionUID = -4218883319932959922L;
+	{
+		add("event");
+		add("eventname");
+		add("activity");
+		add("eventid");
+		add("activityid");
+	}};
+	
+	private static final Set<String> COMPLETION_TIME_COLUMN_IDS = new HashSet<String>() {
+		private static final long serialVersionUID = 6419129336151793063L;
+	{
+		add("completiontime");
+		add("time");
+		add("date");
+		add("enddate");
+		add("timestamp");
+		add("datetime");
+		add("date");
+		add("eventtime");
+		add("tijd");
+		add("datum");
+	}};
 
 	private final class ChangeListenerImpl implements ActionListener {
 
@@ -51,8 +92,8 @@ public final class ConversionConfigUI extends CSVConfigurationPanel implements A
 
 		public void updateSettings() {
 			if (conversionConfig.getCompletionTimeColumn() != null) {
-				conversionConfig.getConversionMap().put(conversionConfig.getCompletionTimeColumn(), new CSVMapping());	
-			}			
+				conversionConfig.getConversionMap().put(conversionConfig.getCompletionTimeColumn(), new CSVMapping());
+			}
 			if (conversionConfig.getStartTimeColumn() != null) {
 				conversionConfig.getConversionMap().put(conversionConfig.getStartTimeColumn(), new CSVMapping());
 			}
@@ -100,32 +141,36 @@ public final class ConversionConfigUI extends CSVConfigurationPanel implements A
 	private final ICSVReader reader;
 	private final CSVPreviewFrame previewFrame;
 	private int maxLoad = 1000;
-		
 
-	public ConversionConfigUI(final CSVFile csv, final CSVConfig importConfig, CSVConversionConfig conversionConfig) throws IOException {		
+	public ConversionConfigUI(final CSVFile csv, final CSVConfig importConfig, CSVConversionConfig conversionConfig)
+			throws IOException {
 		GroupLayout layout = new GroupLayout(this);
-		setLayout(layout);		
+		setLayout(layout);
 		setMaximumSize(new Dimension(COLUMN_WIDTH * 2, Short.MAX_VALUE));
 		reader = csv.createReader(importConfig);
 		headers = reader.readNext();
 		headersInclEmpty = Lists.asList("", headers).toArray(new String[headers.length + 1]);
 		this.conversionConfig = conversionConfig;
 		final ChangeListenerImpl changeListener = new ChangeListenerImpl();
-		
-		layout.setAutoCreateContainerGaps(true);
-		layout.setAutoCreateGaps(true);;
 
-		JLabel standardAttributesLabel = SlickerFactory.instance().createLabel("<HTML><H2>Mapping to Standard XES Attributes</H2></HTML>");
+		layout.setAutoCreateContainerGaps(true);
+		layout.setAutoCreateGaps(true);
+		;
+
+		JLabel standardAttributesLabel = SlickerFactory.instance().createLabel(
+				"<HTML><H2>Mapping to Standard XES Attributes</H2></HTML>");
 		JButton showPreviewButton = new SlickerButton("Toggle Preview");
 		showPreviewButton.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
-				togglePreviewFrame();				
+				togglePreviewFrame();
 			}
 		});
 
 		caseComboBox = new ProMListSortableWithComboBox<>(new DefaultComboBoxModel<>(headers));
-		JLabel caseLabel = createLabel("Case Column (Optional)", "Groups events into traces, and is mapped to 'concept:name' of the trace. Select one or more columns, re-order by drag & drop.");
+		JLabel caseLabel = createLabel(
+				"Case Column (Optional)",
+				"Groups events into traces, and is mapped to 'concept:name' of the trace. Select one or more columns, re-order by drag & drop.");
 		caseComboBox.getListModel().addListDataListener(new ListDataListener() {
 
 			public void intervalRemoved(ListDataEvent e) {
@@ -142,7 +187,8 @@ public final class ConversionConfigUI extends CSVConfigurationPanel implements A
 		});
 
 		eventComboBox = new ProMListSortableWithComboBox<>(new DefaultComboBoxModel<>(headers));
-		JLabel eventLabel = createLabel("Event Column (Optional)", "Mapped to 'concept:name' of the event. Select one or more columns, re-order by drag & drop.");
+		JLabel eventLabel = createLabel("Event Column (Optional)",
+				"Mapped to 'concept:name' of the event. Select one or more columns, re-order by drag & drop.");
 		eventComboBox.getListModel().addListDataListener(new ListDataListener() {
 
 			public void intervalRemoved(ListDataEvent e) {
@@ -163,32 +209,48 @@ public final class ConversionConfigUI extends CSVConfigurationPanel implements A
 		completionTimeColumnCbx.addActionListener(changeListener);
 
 		startTimeColumnCbx = new ProMComboBox<>(headersInclEmpty);
-		JLabel startTimeLabel = createLabel("Start Time (Optional)", "Mapped to 'time:timestamp' of a separate start event");
+		JLabel startTimeLabel = createLabel("Start Time (Optional)",
+				"Mapped to 'time:timestamp' of a separate start event");
 		startTimeColumnCbx.addActionListener(changeListener);
 
-		
 		SequentialGroup verticalGroup = layout.createSequentialGroup();
-		verticalGroup.addGroup(layout.createParallelGroup(Alignment.CENTER)
-				.addComponent(standardAttributesLabel)
+		verticalGroup.addGroup(layout.createParallelGroup(Alignment.CENTER).addComponent(standardAttributesLabel)
 				.addComponent(showPreviewButton));
 		verticalGroup.addGroup(layout.createParallelGroup()
 				.addGroup(layout.createSequentialGroup().addComponent(caseLabel).addComponent(caseComboBox))
 				.addGroup(layout.createSequentialGroup().addComponent(eventLabel).addComponent(eventComboBox)));
-		verticalGroup.addGroup(layout.createParallelGroup()
-				.addGroup(layout.createSequentialGroup().addComponent(completionTimeLabel).addComponent(completionTimeColumnCbx))
-				.addGroup(layout.createSequentialGroup().addComponent(startTimeLabel).addComponent(startTimeColumnCbx)));
-		
+		verticalGroup
+				.addGroup(layout
+						.createParallelGroup()
+						.addGroup(
+								layout.createSequentialGroup().addComponent(completionTimeLabel)
+										.addComponent(completionTimeColumnCbx))
+						.addGroup(
+								layout.createSequentialGroup().addComponent(startTimeLabel)
+										.addComponent(startTimeColumnCbx)));
+
 		ParallelGroup horizontalGroup = layout.createParallelGroup();
-		horizontalGroup.addGroup(layout.createSequentialGroup()
-				.addComponent(standardAttributesLabel)
+		horizontalGroup.addGroup(layout.createSequentialGroup().addComponent(standardAttributesLabel)
 				.addComponent(showPreviewButton));
-		horizontalGroup.addGroup(layout.createSequentialGroup()
-				.addGroup(layout.createParallelGroup().addComponent(caseLabel, COLUMN_WIDTH, COLUMN_WIDTH, COLUMN_WIDTH).addComponent(caseComboBox, COLUMN_WIDTH, COLUMN_WIDTH, COLUMN_WIDTH))
-				.addGroup(layout.createParallelGroup().addComponent(eventLabel, COLUMN_WIDTH, COLUMN_WIDTH, COLUMN_WIDTH).addComponent(eventComboBox, COLUMN_WIDTH, COLUMN_WIDTH, COLUMN_WIDTH)));
-		horizontalGroup.addGroup(layout.createSequentialGroup()
-				.addGroup(layout.createParallelGroup().addComponent(completionTimeLabel, COLUMN_WIDTH, COLUMN_WIDTH, COLUMN_WIDTH).addComponent(completionTimeColumnCbx, COLUMN_WIDTH, COLUMN_WIDTH, COLUMN_WIDTH))
-				.addGroup(layout.createParallelGroup().addComponent(startTimeLabel, COLUMN_WIDTH, COLUMN_WIDTH, COLUMN_WIDTH).addComponent(startTimeColumnCbx, COLUMN_WIDTH, COLUMN_WIDTH, COLUMN_WIDTH)));
-		
+		horizontalGroup.addGroup(layout
+				.createSequentialGroup()
+				.addGroup(
+						layout.createParallelGroup().addComponent(caseLabel, COLUMN_WIDTH, COLUMN_WIDTH, COLUMN_WIDTH)
+								.addComponent(caseComboBox, COLUMN_WIDTH, COLUMN_WIDTH, COLUMN_WIDTH))
+				.addGroup(
+						layout.createParallelGroup().addComponent(eventLabel, COLUMN_WIDTH, COLUMN_WIDTH, COLUMN_WIDTH)
+								.addComponent(eventComboBox, COLUMN_WIDTH, COLUMN_WIDTH, COLUMN_WIDTH)));
+		horizontalGroup.addGroup(layout
+				.createSequentialGroup()
+				.addGroup(
+						layout.createParallelGroup()
+								.addComponent(completionTimeLabel, COLUMN_WIDTH, COLUMN_WIDTH, COLUMN_WIDTH)
+								.addComponent(completionTimeColumnCbx, COLUMN_WIDTH, COLUMN_WIDTH, COLUMN_WIDTH))
+				.addGroup(
+						layout.createParallelGroup()
+								.addComponent(startTimeLabel, COLUMN_WIDTH, COLUMN_WIDTH, COLUMN_WIDTH)
+								.addComponent(startTimeColumnCbx, COLUMN_WIDTH, COLUMN_WIDTH, COLUMN_WIDTH)));
+
 		layout.linkSize(eventLabel, caseLabel);
 		layout.linkSize(completionTimeLabel, startTimeLabel);
 
@@ -211,13 +273,9 @@ public final class ConversionConfigUI extends CSVConfigurationPanel implements A
 		autoDetectCaseColumn();
 		autoDetectEventColumn();
 		autoDetectCompletionTimeColumn();
-		autoDetectDataTypes(csv, conversionConfig.getConversionMap());
-		
-		changeListener.updateSettings();
-	}
+		autoDetectDataTypes(csv, conversionConfig.getConversionMap(), importConfig);
 
-	private void autoDetectDataTypes(CSVFile csv, Map<String, CSVMapping> conversionMap) {
-		
+		changeListener.updateSettings();
 	}
 
 	private void togglePreviewFrame() {
@@ -229,7 +287,7 @@ public final class ConversionConfigUI extends CSVConfigurationPanel implements A
 			} catch (Exception e) {
 				JOptionPane.showMessageDialog(this, "Error parsing CSV " + e.getMessage(), "CSV Parsing Error",
 						JOptionPane.ERROR_MESSAGE);
-			}	
+			}
 		} else {
 			previewFrame.setVisible(false);
 		}
@@ -239,8 +297,7 @@ public final class ConversionConfigUI extends CSVConfigurationPanel implements A
 		for (int i = 0; i < headers.length; i++) {
 			String header = headers[i];
 
-			if ("case".equalsIgnoreCase(header) || "trace".equalsIgnoreCase(header)
-					|| "traceid".equalsIgnoreCase(header) || "caseid".equalsIgnoreCase(header)) {
+			if (CASE_COLUMN_IDS.contains(header.toLowerCase(Locale.US).trim())) {
 				caseComboBox.addElement(header);
 				return;
 			}
@@ -251,9 +308,7 @@ public final class ConversionConfigUI extends CSVConfigurationPanel implements A
 		for (int i = 0; i < headers.length; i++) {
 			String header = headers[i];
 
-			if ("event".equalsIgnoreCase(header) || "eventname".equalsIgnoreCase(header)
-					|| "activity".equalsIgnoreCase(header) || "eventid".equalsIgnoreCase(header)
-					|| "activityid".equalsIgnoreCase(header)) {
+			if (EVENT_COLUMN_IDS.contains(header.toLowerCase(Locale.US).trim())) {
 				eventComboBox.addElement(header);
 				return;
 			}
@@ -264,12 +319,38 @@ public final class ConversionConfigUI extends CSVConfigurationPanel implements A
 		for (int i = 0; i < headers.length; i++) {
 			String header = headers[i];
 
-			if ("time".equalsIgnoreCase(header) || "tijd".equalsIgnoreCase(header)
-					|| "timestamp".equalsIgnoreCase(header) || "endtime".equalsIgnoreCase(header)
-					|| "completiontime".equalsIgnoreCase(header) || "date".equalsIgnoreCase(header)
-					|| "datetime".equalsIgnoreCase(header) || "eventtime".equalsIgnoreCase(header)) {
+			if (COMPLETION_TIME_COLUMN_IDS.contains(header.toLowerCase(Locale.US).trim())) {
 				completionTimeColumnCbx.setSelectedItem(header);
 				return;
+			}
+		}
+	}
+
+	private void autoDetectDataTypes(CSVFile csv, Map<String, CSVMapping> conversionMap, CSVConfig csvConfig) throws IOException {
+		try (ICSVReader reader = csv.createReader(csvConfig)) {
+			String[] header = reader.readNext();
+			Map<String, List<String>> valuesPerColumn = new HashMap<>();
+			for (String h : header) {
+				valuesPerColumn.put(h, new ArrayList<String>());
+			}
+			// now read 10 lines or so to guess the data type
+			for (int i = 0; i < DATA_TYPE_FORMAT_AUTO_DETECT_NUM_LINES; i++) {
+				String[] cells = reader.readNext();
+				if (cells == null) {
+					break;
+				}
+				for (int j = 0; j < cells.length; j++) {
+					List<String> values = valuesPerColumn.get(header[j]);
+					values.add(values.get(j));
+					valuesPerColumn.put(header[j], values);
+				}
+			}
+			// now we can guess the data type
+			for (String h : header) {
+				List<String> values = valuesPerColumn.get(h);
+				// now we can guess the type
+				// let's try the discrete values
+				
 			}
 		}
 	}
